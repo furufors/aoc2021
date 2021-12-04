c6ad46d2d2dd818d55c5ee16c42786e628dd9de8
FFURUFOR <fredrik.furufors@volvocars.com>
2021-12-04 12:50:36 +0100

    Added day 4

---
+++ day4/day4p1.hs
@@ -0,0 +1,61 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-18.18 script
+import Data.List (transpose)
+data Cell = Hit | Open Int deriving Show
+type Board = [[Cell]]
+type Callout = [Int]
+type GameStatus = (Callout, [Board])
+
+main :: IO ()
+main = interact $ show . play . parsein
+    where
+        play :: GameStatus -> Int
+        play ([  ],  _) = error "No winner"
+        play (ns0, bs0) = if winner bs0
+                          then score (head ns0) bs0
+                          else play . gameStep $ (tail ns0, bs0)
+
+        winner :: [Board] -> Bool
+        winner = any solved
+
+        solved :: Board -> Bool
+        solved b = let h = map (map toHit) b 
+                   in row h || col h
+
+        row = any (== 5) . map sum
+        col = row . transpose
+
+        gameStep ::  GameStatus -> GameStatus
+        gameStep (ns, bs) = (ns, map (map (map (is (head ns)))) bs)
+        
+        is :: Int -> Cell -> Cell
+        is n Hit = Hit
+        is n (Open i) = if n == i then Hit else Open i
+        
+        score :: Int -> [Board] -> Int
+        score n [    ] = error "No winner"
+        score n (x:xs) = if solved x
+                         then n * getScore x
+                         else score n xs
+        
+        getScore :: Board -> Int
+        getScore = sum . map (sum . (map toI))
+
+        toI, toHit :: Cell -> Int
+        toI        Hit = 0
+        toI   (Open i) = i
+        toHit      Hit = 1
+        toHit (Open _) = 0
+
+        parsein :: String -> GameStatus
+        parsein = p1 . map (map read . words) . filter (/= []) . lines . map repl
+            where
+                repl :: Char -> Char
+                repl ',' = ' '
+                repl   x = x
+                p1 xs = (head xs, chunks 5 . map (map Open) $ tail xs)
+                chunks :: Int -> [a] -> [[a]]
+                chunks _ [] = []
+                chunks n l
+                  | n > 0 = (take n l) : (chunks n (drop n l))
+                  | otherwise = error "Chunking issue"
