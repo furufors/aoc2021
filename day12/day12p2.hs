fa6a8428d1ca06d1715d4af72b10e9931eb1b6c2
FFURUFOR <fredrik.furufors@volvocars.com>
2021-12-12 13:11:08 +0100

    Added day 12

--- day12/day12p2.hs
+++ day12/day12p2.hs
@@ -1,13 +1,7 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
-import Data.List (group, sort, reverse)
-import Debug.Trace (trace)
+import Data.List (group, sort)
 data Cave = Start | Large String | Small String | End deriving (Eq, Ord)
-instance Show Cave where
-    show Start = "Start"
-    show (Small a) =  a
-    show (Large a) =  a
-    show End = "End"
 type Step = (Cave, Cave) -- (From, To)
 
 main = interact $ show . length . generatePaths . parsein
@@ -28,18 +22,20 @@ main = interact $ show . length . generatePaths . parsein
         getStart (x:xs) = if contains Start x then [x]:getStart xs else getStart xs
 
         addStep :: [Step] -> [[Step]] -> [[Step]]
-        -- addStep sys routes = trace (showPaths routes) $ uniq $ 
         addStep sys routes = uniq $ 
                              [if isDone route then route else direction e (head route):route
                              | route <- routes
                              , e <- sys
                              , isDone route || continuous e (head route)
-                             , isDone route || notVisited route (direction e (head route)) || targetIsLarge (direction e (head route))
+                             , isDone route || visited ((direction e (head route)):route)
                              , isDone route || not (contains Start e)]
             where
-                notVisited ss (_,t) = not (any (contains t) ss)
-                targetIsLarge (_, Large _) = True
-                targetIsLarge (_, _      ) = False
+                visited = (\x -> maxOneMulti x && noMoreThanDouble x) . map length . group . sort . toListOfSmallVisited
+                maxOneMulti = (<2) . length . filter (>=2)
+                noMoreThanDouble = all (<3)
+                toListOfSmallVisited [        ] = []
+                toListOfSmallVisited ((f, (Small t)):ss) = (Small t):toListOfSmallVisited ss
+                toListOfSmallVisited ((f,         t):ss) = toListOfSmallVisited ss
                 uniq = map head . group . sort
                 continuous (a,b) (_,d) = elem d [a,b]
                 isDone = (==End) . snd . head
@@ -58,7 +54,4 @@ main = interact $ show . length . generatePaths . parsein
                 makeStep' "end"   = End
                 makeStep' "start" = Start
                 makeStep' x       = if head x `elem` ['A'..'Z'] then Large x else Small x
-
-        showPaths :: [[Step]] -> String
-        showPaths = ("-----\n"++) . foldl1 (\a b -> a ++ "\n" ++ b) . map (foldl1 (\a b -> a ++ "->" ++ b) . map show . reverse)
         
