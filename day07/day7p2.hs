8e35d8c7836d9412f63891e063e132e55a2fa893
Fredrik Furufors <furufors@gmail.com>
2022-01-11 16:42:50 +0100

    Day 7 added

--- day07/day7p2.hs
+++ day07/day7p2.hs
@@ -7,8 +7,11 @@ import Data.Ord (comparing)
 main = interact $ show . minfuel . calculate . parsein
     where
         minfuel :: [(Int,Int)] -> Int
-        minfuel = snd . head . sortBy (comparing snd)
+        minfuel = snd . head .sortBy (comparing snd)
         calculate :: [Int] -> [(Int,Int)]
-        calculate ps = map (\p -> (p, sum [abs (p-p') | p' <- ps])) [minimum ps..maximum ps]
+        calculate ps = map (\p -> (p, sum [cost (abs (p-p')) | p' <- ps])) [minimum ps..maximum ps]
+        cost :: Int -> Int
+        cost 0 = 0
+        cost n = n + cost (n-1)
         parsein :: String -> [Int]
         parsein = map read . words . map (\case ',' -> ' '; x -> x)
